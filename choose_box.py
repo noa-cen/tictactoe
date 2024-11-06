5b8f10c172a3769f06a8e74dc77136da2e9b6912
AdelinePat <adeline.patenne@hotmail.fr>
2024-11-06 11:20:07 +0100

    ajout de la boucle pour entrer sa case tant que l'input n'est pas bon

--- choose_box.py
+++ choose_box.py
@@ -9,33 +9,44 @@ def choose_box():
     
     match box:
         case "A1" | "1A" | "a1" | "1a":
+            box_choice_ok = True
             coordonate["row"] = 1
             coordonate["column"] = 1
         case "A2" | "2A" | "a2" | "2a":
+            box_choice_ok = True
             coordonate["row"] = 2
             coordonate["column"] = 1
         case "A3" | "3A" | "a3" | "3a":
+            box_choice_ok = True
             coordonate["row"] = 3
             coordonate["column"] = 1
         case "B1" | "1B" | "b1"  | "1b":
+            box_choice_ok = True
             coordonate["row"] = 1
             coordonate["column"] = 2
         case "B2" | "2B" | "b2" | "2b":
+            box_choice_ok = True
             coordonate["row"] = 2
             coordonate["column"] = 2
         case "B3" | "3B" | "b3" | "3b":
+            box_choice_ok = True
             coordonate["row"] = 3
             coordonate["column"] = 2
         case "C1" | "1C" | "c1" | "1c":
+            box_choice_ok = True
             coordonate["row"] = 1
             coordonate["column"] = 3
         case "C2" | "2C" | "c2" | "2c":
+            box_choice_ok = True
             coordonate["row"] = 2
             coordonate["column"] = 3
         case "C3" | "3C" | "c3" | "3c":
+            box_choice_ok = True
             coordonate["row"] = 3
             coordonate["column"] = 3
         case _:
             print("Les coordonnées ne sont pas bonne, veuillez les entrer à nouveau.")
-            return choose_box()
+            box_choice_ok = False
+            while box_choice_ok == False:
+                return choose_box()
     return coordonate
