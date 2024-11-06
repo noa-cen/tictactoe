512b3cf78921b2a3db369e5c20f60b2fe422335b
noa-cen <noa.kreins@laplateforme.io>
2024-11-06 15:56:13 +0100

    player_turn

--- player_turn.py
+++ player_turn.py
@@ -1,8 +1,12 @@
 # Print the sign of the payer :
-def player_turn(board, coordonate, player):
+def player_turn(board, coordonate, sign1, sign2):
     if is_box_empty(board, coordonate) == True:
+        for n in range(10):
+            if n % 2 == 1:
+                player = sign1
+            elif n % 2 == 0:
+                player = sign2
         coordonate = player
-        return board
     else:
         print("Case déjà occupée, choisis une autre case.")
         return choose_box()
