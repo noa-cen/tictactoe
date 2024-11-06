dd1b65afa7e5cb46b33a26585e000ece94520ffc
noa-cen <noa.kreins@laplateforme.io>
2024-11-06 15:35:54 +0100

    introduction

---
+++ intro.py
@@ -0,0 +1,13 @@
+# Introduction to the game and rules:
+def intro():
+    print("Bienvenue sur le Tic Tac Toe d'Adeline, Maéva et Noa !")
+    print("\n")
+    print("Règles du jeu: joueur 1 et joueur 2, représentés par X et O, "
+          "choisissent une case à tour de rôle. "
+          "Le premier qui réussit à aligner trois X ou trois O verticalement, "
+          "horizontalement ou en diagonale, gagne la partie."
+          "Le choix de la case se fait sous format 'A3', 'B1', 'C2'...")
+    print("\n")
+    print("Let's play !")
+    print("\n")
+    input("Touche entrée pour continuer.")
