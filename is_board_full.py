72cd43517a78671bef8d27b87eee3f1780b4b4ed
noa-cen <noa.kreins@laplateforme.io>
2024-11-06 15:39:25 +0100

    is_board_full

---
+++ is_board_full.py
@@ -0,0 +1,11 @@
+# This function return if the board is full:
+def is_board_full():
+    count = 1
+    while count < 10:
+        if count == 9:
+            full = True
+            print("Match nul !")
+        else:
+            full = False
+        count += 1
+    return full
